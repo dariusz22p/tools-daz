23583cdc8c4637db9c60707aff244fe1b181c326
Dariusz Panasiuk <Dariusz.Panasiuk@domesticandgeneral.com>
2025-12-07 17:11:39 +0000

    v3

--- MacBook/compress-foty-i-video-v3.sh
+++ MacBook/compress-foty-i-video-v3.sh
@@ -2,8 +2,8 @@
 #!/usr/bin/env bash
 
 # Script metadata
-SCRIPT_URL="https://raw.githubusercontent.com/dariusz22p/tools-daz/main/MacBook/compress-foty-i-video-v2.s"
-SCRIPT_NAME="compress-foty-i-video-v2.s"
+SCRIPT_URL="https://raw.githubusercontent.com/dariusz22p/tools-daz/main/MacBook/compress-foty-i-video-v3.sh"
+SCRIPT_NAME="compress-foty-i-video-v3.sh"
 SCRIPT_VERSION="2.2.0"  # Update this when making changes
 
 # Auto-update: Try to pull latest version from GitHub
